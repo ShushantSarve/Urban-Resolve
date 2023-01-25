e6b31f4a9f6ecf577129ee0ec62d5e6d3f77d9df
Shushant <shushantsarve05@gmail.com>
2023-01-25 21:56:01 +0530

    updated default news image in News.js

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./News.css";
+import defaultimg from '../Utilities/defaultNews.jpg'
 
 
 function News() {
 
-  const url ="https://newsapi.org/v2/everything?q=pollution&apikey=226cb84b76db43dcb0b55516711accb2&pageSize=6";
+  const url ="https://newsdata.io/api/1/news?apikey=pub_159762382799484f8db98c7c04a8422ce3596&q=pollution&country=in";
 
   const [articles, setArticles] = useState([]);
   
@@ -13,7 +14,7 @@ function News() {
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setArticles(data.articles);
+      setArticles(data.results);
     } catch (error) {
       console.log(error);
     }
@@ -25,8 +26,8 @@ function News() {
   }, []);
   
   
-  const firstThreeArticles = articles?articles.slice(0,3):'NULL';
-  const nextThreeArticles = articles?articles.slice(3):'NULL';
+  const firstThreeArticles = articles.slice(0,3);
+  const nextThreeArticles = articles.slice(3,6);
 
 
   return (
@@ -37,10 +38,10 @@ function News() {
             return(
               <div className="container">
                 <div className="row">
-                  <img className="newsImg" src={element.urlToImage} alt='newsimage'/>
+                  <img className="newsImg" src={element.image_url?element.image_url: defaultimg} alt='newsimage'/>
                 <div className="col">
-                  <a href={element.url} target="_blank" rel="noreferrer">{element.title}</a>
-                  <p className="newsSource">{element.source.name? element.source.name: "Source unknown"}</p>
+                  <a href={element.link} target="_blank" rel="noreferrer">{element.title}</a>
+                  <p className="newsSource">{element.source_id? element.source_id: "Source unknown"}</p>
                 </div>
                 </div>
               </div>
@@ -54,10 +55,10 @@ function News() {
             return(
               <div className="container">
                 <div className="row">
-                  <img className="newsImg" src={element.urlToImage} alt='newsimage'/>
+                  <img className="newsImg" src={element.image_url?element.image_url: defaultimg} alt='newsimage'/>
                 <div className="col">
-                  <a href={element.url} target="_blank" rel="noreferrer">{element.title}</a>
-                  <p className="newsSource">{element.source.name? element.source.name: "Source unknown"}</p>
+                  <a href={element.link} target="_blank" rel="noreferrer">{element.title}</a>
+                  <p className="newsSource">{element.source_id? element.source_id: "Source unknown"}</p>
                 </div>
                 </div>
               </div>
